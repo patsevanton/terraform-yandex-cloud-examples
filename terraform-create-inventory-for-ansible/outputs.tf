dddc0221332708e04b645dd2b604d61f7f802bb8
Anton Patsev <apatsev@luxoft.com>
2020-11-02 14:00:22 +0600

    Add vm to inventory.tmpl

--- terraform-create-inventory-for-ansible/outputs.tf
+++ terraform-create-inventory-for-ansible/outputs.tf
@@ -1,9 +1,15 @@
 resource "local_file" "AnsibleInventory" {
   content = templatefile("inventory.tmpl",
     {
-      name                     = yandex_compute_instance.vm-2.name,
-      hostname_vm_1            = yandex_compute_instance.vm-2.hostname,
-      external_ip_address_vm_1 = yandex_compute_instance.vm-1.network_interface.0.nat_ip_address,
+      vm_1_name                = yandex_compute_instance.vm-1.name,
+      vm_1_hostname            = yandex_compute_instance.vm-1.hostname,
+      vm_1_external_ip_address = yandex_compute_instance.vm-1.network_interface.0.nat_ip_address,
+      vm_1_internal_ip_address = yandex_compute_instance.vm-1.network_interface.0.ip_address,
+      
+      vm_2_name                = yandex_compute_instance.vm-2.name,
+      vm_2_hostname            = yandex_compute_instance.vm-2.hostname,
+      vm_2_external_ip_address = yandex_compute_instance.vm-2.network_interface.0.nat_ip_address,
+      vm_2_internal_ip_address = yandex_compute_instance.vm-2.network_interface.0.ip_address,
     }
   )
   filename = "inventory.ini"
