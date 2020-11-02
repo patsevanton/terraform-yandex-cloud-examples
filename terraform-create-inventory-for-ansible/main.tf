142e2ec354755b2bce2e5bd040e7fdab21c5e760
Anton Patsev <patsev.anton@gmail.com>
2020-11-02 13:30:13 +0600

    add terraform-create-inventory-for-ansible

--- terraform-create-inventory-for-ansible/main.tf
+++ terraform-create-inventory-for-ansible/main.tf
@@ -80,3 +80,4 @@ output "external_ip_address_vm_1" {
 output "external_ip_address_vm_2" {
   value = yandex_compute_instance.vm-2.network_interface.0.nat_ip_address
 }
+
---
+++ terraform-create-inventory-for-ansible/outputs.tf
@@ -0,0 +1,10 @@
+resource "local_file" "AnsibleInventory" {
+ content = templatefile("inventory.tmpl",
+ {
+  internal_ip_address_vm_1 = yandex_compute_instance.vm-1.network_interface.0.ip_address,
+  internal_ip_address_vm_2 = yandex_compute_instance.vm-2.network_interface.0.ip_address
+ }
+ )
+ filename = "inventory"
+}
+
