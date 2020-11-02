0c42d54fb0d7ae3f6447da1e5659a200bb591ac3
Anton Patsev <apatsev@luxoft.com>
2020-11-02 16:07:15 +0600

    Change vm_1_name to application1_name

--- terraform-create-inventory-for-ansible/outputs.tf
+++ terraform-create-inventory-for-ansible/outputs.tf
@@ -2,12 +2,10 @@ resource "local_file" "AnsibleInventory" {
   content = templatefile("inventory.tmpl",
     {
       application1_name                   = yandex_compute_instance.application1.name,
-      application1_hostname               = yandex_compute_instance.application1.hostname,
       application1_external_ip_address    = yandex_compute_instance.application1.network_interface.0.nat_ip_address,
       application1_internal_ip_address    = yandex_compute_instance.application1.network_interface.0.ip_address,
       
       patroni_name                = yandex_compute_instance.patroni.*.name,
-      patroni_hostname            = yandex_compute_instance.patroni.*.hostname,
       patroni_external_ip_address = yandex_compute_instance.patroni.*.network_interface.0.nat_ip_address,
       patroni_internal_ip_address = yandex_compute_instance.patroni.*.network_interface.0.ip_address,
     }
