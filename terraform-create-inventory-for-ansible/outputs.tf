9f34ee615a2d11fa007c59ba92e5985954a3d65a
Anton Patsev <patsev.anton@gmail.com>
2020-11-06 12:38:37 +0600

    Disable application1 in output.tf

--- terraform-create-inventory-for-ansible/outputs.tf
+++ terraform-create-inventory-for-ansible/outputs.tf
@@ -1,9 +1,9 @@
 resource "local_file" "AnsibleInventory" {
   content = templatefile("inventory.tmpl",
     {
-      application1_name                   = yandex_compute_instance.application1.name,
-      application1_external_ip_address    = yandex_compute_instance.application1.network_interface.0.nat_ip_address,
-      application1_internal_ip_address    = yandex_compute_instance.application1.network_interface.0.ip_address,
+      # application1_name                   = yandex_compute_instance.application1.name,
+      # application1_external_ip_address    = yandex_compute_instance.application1.network_interface.0.nat_ip_address,
+      # application1_internal_ip_address    = yandex_compute_instance.application1.network_interface.0.ip_address,
       
       # zookeeper_name                = yandex_compute_instance.zookeeper.*.name,
       # zookeeper_external_ip_address = yandex_compute_instance.zookeeper.*.network_interface.0.nat_ip_address,
