210717d93d10f6656ecdc95c105efa6179f66b0c
Anton Patsev <10828883+patsevanton@users.noreply.github.com>
2020-11-06 09:55:27 +0600

    remove ansible-galaxy install juju4.ipv6 from run.sh

--- terraform-create-inventory-for-ansible/run.sh
+++ terraform-create-inventory-for-ansible/run.sh
@@ -5,7 +5,6 @@ time terraform apply -auto-approve
 sleep 60
 
 ansible-playbook disable_selinux.yml
-ansible-galaxy install juju4.ipv6
 ansible-galaxy install andrewrothstein.zookeeper-cluster
 # https://github.com/andrewrothstein/ansible-zookeeper-cluster/issues/9
 ansible-galaxy install andrewrothstein.zookeeper --force
